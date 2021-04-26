0f28a2cd2da7916b6163c43aa9505e38e33e99f6
mostafa <m.rostamimahmodi72@gmail.com>
2021-04-26 21:54:08 +0430

    Updates

---
+++ precache-manifest.8e7c3af1cbb90892ca84dbd23c0c49c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d786519679856aa33ad9b5d2f36b6d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "783cec3dd0163e1cb984",
+    "url": "./static/css/2.9ee52aab.chunk.css"
+  },
+  {
+    "revision": "1c5edb0ade17a0bbfebb",
+    "url": "./static/css/main.67b9d217.chunk.css"
+  },
+  {
+    "revision": "783cec3dd0163e1cb984",
+    "url": "./static/js/2.819e50d8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.819e50d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5edb0ade17a0bbfebb",
+    "url": "./static/js/main.6a175f38.chunk.js"
+  },
+  {
+    "revision": "dbd337cbd970fbe9db76",
+    "url": "./static/js/runtime-main.0ccf0e8e.js"
+  }
+]);
